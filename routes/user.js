2961aed6bf5f8ebab655f819900b95631544a121
Rafika <rafikatijani@outlook.de>
2024-02-20 13:32:02 +0100

    login with bcrypt, JWT and Testing with thunderClient.

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import {
+  deleteUser,
+  getUsers,
+  login,
+  register,
+  updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/", getUsers);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
