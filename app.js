f53b0d68b5a103ecebd03c4d91c2f67e3a1f9d82
KassandraJK <jenskoeniger196@googlemail.com>
2024-02-29 13:52:53 +0100

    App.js update

--- app.js
+++ app.js
@@ -6,26 +6,53 @@ import cors from "cors";
 import userRouter from "./routes/user.js";
 import roleRouter from "./routes/role.js";
 import friendRouter from "./routes/friend.js";
-//import serverRouter from "./routes/server.js";
 import blockedUserRouter from "./routes/blockedUser.js";
+import { Server } from "socket.io";
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
+
+//import serverRouter from "./routes/server.js";
 /* import { seedRoles } from "./services/seed.services.js"; */
+
 //* Port for the server
 const PORT = process.env.PORT || 4000;
 
 //* URI for the database
-const URI = process.env.DB_URI;
+const dbURI = process.env.DB_URI;
 
 const app = express();
 
+const socketServer = http.createServer(app);
+
+const io = new Server(socketServer, {
+  cors: {
+    origin: process.env.CLIENT || "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+//* create the socket connection
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
 //* mongoose connection to the database
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.79l9asu.mongodb.net/chat_app?retryWrites=true&w=majority` //`mongodb://localhost:27017/chat_app`, //URI
-  )
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.log(err));
+  .connect(dbURI)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 mongoose.connection.on("error", (err) => {
-  console.log("err", err);
+  console.log("an error:", err);
 });
 
 //* Middlewares for the server
@@ -34,7 +61,7 @@ app.use(express.json()); //body parser
 app.use(
   cors({
     origin: process.env.CLIENT || "http://localhost:5173",
-    allowedHeaders: 'Content-Type',
+    allowedHeaders: "Content-Type",
     credentials: true,
   })
 );
@@ -48,6 +75,18 @@ app.use("/blockedUser", blockedUserRouter);
 /* const roles = await seedRoles(); */
 
 //* Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`HTTP Server is running on port ${PORT}`);
+  }
+});
+
+socketServer.listen(4001, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Socket server is running on port 4001`);
+  }
 });
